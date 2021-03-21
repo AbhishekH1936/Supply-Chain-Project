1888b00dcb6a9a68e9d79b8f994e190b8d54129a
Nikhil483 <cnikhil483@gmail.com>
2021-03-21 16:29:19 +0530

    smart contract minor updates

--- src/contracts/scm.sol
+++ src/contracts/scm.sol
@@ -1,28 +1,62 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract Scm {
+    
+  constructor() public {
+     // ACfees[2] = 39/1000;
+      
+  }
+    
+  // User data storage attributes
   mapping(string => string) signup;
   string[] usernames_signup = ["0x743a264D199d1133c3D3Eac18B91BB8E568b09Ea-admin"];
   string[] AdminDetails = ["0x743a264D199d1133c3D3Eac18B91BB8E568b09Ea-admin","Pass123@"];
   
   
+  // Crop data storage attributes
   mapping(string => string[]) farmer_crops;
   mapping(string => string) cropInfo;
   string[] allCropId = ["a"];
   
-  uint256 public bal;
+  
+  // Ether stored in contract address
+  uint256 public contractBalance;
+  
+  
+  // attribute to map farmer to his security deposit
   mapping(address => uint256) securityDeposit;
   
-  function setSecurityDeposit () external payable {
-      securityDeposit[msg.sender] = securityDeposit[msg.sender] + msg.value;
-      bal=address(this).balance;
+  // agroConsultant fees mapping
+  mapping(uint256 => ) ACfees;
+  
+  
+  // Getter and setter for agroConsultant fees
+  function setACfees(uint256 experience, uint256 fees) public {
+      ACfees[experience] = fees;
+  }
+  function getACfees(uint256 experience) public view returns (uint256) {
+      return ACfees[experience];
   }
   
+  
+  // get contractBalance
+  function getContractBalance() public view returns (uint256) {
+      return contractBalance;
+  }
+  
+  
+  // Getter and setters for securityDeposit
+  function setSecurityDeposit () external payable {
+      securityDeposit[msg.sender] = securityDeposit[msg.sender] + msg.value;
+      contractBalance=address(this).balance;
+  }
   function getSecurityDeposit () public view returns (uint256){
       return securityDeposit[msg.sender];
   }
   
+  // function checking for Admin login
   function validateAdmindetails(string memory pub_key, string memory pass) public view returns (bool) {
       if(keccak256(bytes(pub_key)) == keccak256(bytes(AdminDetails[0])) && keccak256(bytes(pass))==keccak256(bytes(AdminDetails[1]))){
           return true; 
@@ -30,6 +64,8 @@ contract Scm {
       return false;
   }
   
+  
+  // setters and getters for Farmer's crops, by different parameters
   function setFarmerCrops(string memory pub_key, string memory crop_hash, string memory crop_id) public {
       string[] storage cropArray = farmer_crops[pub_key];
       cropArray.push(crop_hash);
@@ -39,19 +75,30 @@ contract Scm {
       
       allCropId.push(crop_id);
   }
-  
   function getFarmerCrops(string memory pub_key) public view returns(string[] memory){
       return farmer_crops[pub_key];
   }
-  
   function getCropByCropId(string memory crop_id) public view returns(string memory){
       return cropInfo[crop_id];
   }
-  
   function getAllCropId() public view returns(string[] memory){
       return allCropId;
   }
   
+  
+  // getters and setters for user registration
+  function set_signup(string memory pub_key, string memory signup_hash) public {
+    signup[pub_key] = signup_hash;
+    usernames_signup.push(pub_key);
+  }
+  function get_signup(string memory pub_key) public view returns (string memory) {
+    return signup[pub_key];
+  }
+  function get_usernames() public view returns (string[] memory) {
+    return usernames_signup;
+  }
+  
+  // functions checking for matching Id's
   function match_cropId(string memory crop_id) public view returns (bool){
     uint256 flag = 0;
     for(uint i=0; i< allCropId.length; i++){
@@ -66,21 +113,6 @@ contract Scm {
     }
     return false;
   }
-
-
-  function set_signup(string memory pub_key, string memory signup_hash) public {
-    signup[pub_key] = signup_hash;
-    usernames_signup.push(pub_key);
-  }
-
-  function get_signup(string memory pub_key) public view returns (string memory) {
-    return signup[pub_key];
-  }
-  
-  function get_usernames() public view returns (string[] memory) {
-    return usernames_signup;
-  }
-
   function match_usernames(string memory pub_key) public view returns (bool){
     uint256 flag = 0;
     for(uint i=0; i< usernames_signup.length; i++){
